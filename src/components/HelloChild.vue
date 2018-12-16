21b17c1a440d8d57d3658ee94d8ee70ada6d9e90
kei <kei.takakuda.0415@icloud.com>
2018-12-16 16:22:11 +0900

    create HelloChild.vue

---
+++ src/components/HelloChild.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>{{title}}</p>
+    <!-- ここでpropsに設定された値をそのまま表示する -->
+    <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      title: 'はろーこども！'
+    }
+  },
+  props: ['message']
+}
+</script>
+
